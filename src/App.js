8f1e511b0f53d14f808bbf8ad47f56d2039420fe
Christopher Miller <miller1997@gmail.com>
2018-10-24 15:35:44 -0400

    Basic Flash Cards Done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import DrawButton from './Components/DrawButton/DrawButton'
+import Card from './Components/Card/Card';
+import firebase from 'firebase/app';
+import 'firebase/database';
+import 'tachyons';
 
+import {DB_CONFIG} from './config/firebase/db_config';
 class App extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.app = firebase.initializeApp(DB_CONFIG);
+    this.database = this.app.database().ref().child('cards')
+    this.updateCard = this.updateCard.bind(this);
+
+    this.state = {
+      cards: [
+        
+      ],
+      currentCard:{}
+    }
+  }
+
+  componentWillMount(){
+    const currentCards = this.state.cards;
+    this.database.on('child_added',snap=>{
+      currentCards.push(
+        {
+          id:snap.key,
+          term:snap.val().term,
+          definition:snap.val().definition
+        }
+      )
+      this.setState(
+        {
+          cards:currentCards,
+          currentCard:this.getRandomCard(currentCards)
+        }
+      )
+    }
+  
+  )
+    
+  }
+
+  getRandomCard(currentCards){
+    var card = currentCards[Math.floor(Math.random() * currentCards.length)];
+    return card;
+  }
+  updateCard(){
+    const currentCards = this.state.cards;
+    this.setState(
+      {
+        currentCard:this.getRandomCard(currentCards)
+      }
+    )
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="CardRow">
+      <Card term={this.state.currentCard.term} definition={this.state.currentCard.definition}/>
+      </div>
+        <div className="ButtonRow">
+        <DrawButton drawCard={this.updateCard}/>
+        </div>
+        
       </div>
     );
   }
---
+++ src/Components/Card/Card.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Card.css'
+
+const Card = (props) =>(
+    <div className="card-container  ">
+        <div className="card">
+            <div className="front">
+                <div className="term">
+                    {props.term}
+                </div>
+            </div>
+            <div className="back">
+                <div className="definition">
+                     {props.definition}
+                </div>
+            </div>
+        </div>
+        
+    </div>
+);
+
+export default Card;
